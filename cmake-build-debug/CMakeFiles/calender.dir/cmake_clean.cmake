e34ec8c82f54736db38e924e8313ecfeef3fb98f
zywzstbs <1493988058@qq.com>
2025-05-22 17:14:36 +0800

    完成大部分功能

---
+++ cmake-build-debug/CMakeFiles/calender.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calender.dir/main.cpp.o"
+  "CMakeFiles/calender.dir/main.cpp.o.d"
+  "calender"
+  "calender.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Volumes/KIOXIA/code/C/calender
+# Install script for directory: /Users/zhangyunwen/Documents/code/C/calender
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Volumes/KIOXIA/code/C/calender/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "/Users/zhangyunwen/Documents/code/C/calender/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Volumes/KIOXIA/code/C/calender/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/zhangyunwen/Documents/code/C/calender/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
